230eac05817d0fcad1ea1204784c537a26396e8c
mmriz16 <m.miftakhul.rizky4@gmail.com>
2022-06-17 16:40:56 +0700

    Add dashboard

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Not Found</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-screen">
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-lg w-full space-y-8">
+            <div class="flex justify-center">
+                <img class="mb-4" src="just_logo.svg" width="80">
+            </div>
+            <div>
+                <h2 class="text-center text-md font-semibold text-indigo-500">404 ERROR</h2>
+                <h2 class="text-center text-5xl font-extrabold text-gray-900">Page not found.</h2>
+                <h2 class="mt-4 text-center text-lg text-gray-900">Sorry, we couldn't find the page you're looking for.</h2>
+                <a class="text-center" href="/"><p class="mt-10 text-indigo-500 font-semibold">Go back home</p></a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
