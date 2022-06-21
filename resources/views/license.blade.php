8c77a41faede0d8e273e1619cbcd6dc756654fdc
mmriz16 <m.miftakhul.rizky4@gmail.com>
2022-06-21 14:30:23 +0700

    Add license content

--- resources/views/license.blade.php
+++ resources/views/license.blade.php
@@ -1,4 +1,23 @@
 @extends('layout.main')
 @section('container')
-    <h1>License</h1>
+<div class="d-flex justify-content-center align-items-center" style="height: calc(100vh - 60px - 100px)">
+    <div class="col-5 my-auto">
+        <h2 style=" font-weight: bold; color: var(--black);">Termicons Free License</h2>
+        <p style="color: var(--black-50)">Last updated on January 31, 2021</p>
+        <p class="pt-5" style="color: var(--black-70);">Termicons is free, open source, and GPL friendly. You can use it
+            for commercial projects,
+            open source projects, or anywhere you want.</p>
+        <p class="pt-3" style="font-weight: bold; margin-bottom: 0; color: var(--black);">Icons : CC BY 4.0 License</p>
+        <p class="pt-2" style="color: var(--black-70);">In the Termicons Free download, the CC BY 4.0 license applies to
+            all icons packaged as SVG and JS
+            file types.</p>
+        <p class="pt-3" style="font-weight: bold; margin-bottom: 0; color: var(--black);">Attribution</p>
+        <p class="pt-2" style="color: var(--black-70);">Attribution is required by CC BY licenses. Downloaded Termicons
+            Free files already contain
+            embedded comments with sufficient attribution, you aren't required to do anything more.
+            We've kept attribution comments short and precise, so we ask that you do not actively work to
+            remove them from files, especially code. They're a great way for people to learn about
+            Termicons.</p>
+    </div>
+</div>
 @endsection
