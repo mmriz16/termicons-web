c03df8b228f1f88c7c0a5f17ff9cd7f65f4de58f
mmriz16 <m.miftakhul.rizky4@gmail.com>
2022-06-24 21:22:51 +0700

    add more content cheatsheet

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#0F58EB">
-    <title>Termicons | Title</title>
+    <title>Termicons | {{ $title }}</title>
     <link rel="icon" href="<?= url('just_logo.svg') ?>">
 
     <!-- CSS -->
