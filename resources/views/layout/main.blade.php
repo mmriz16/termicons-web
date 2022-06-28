09786b3b8f4bbdb600a427c56931800826839db5
mmriz16 <m.miftakhul.rizky4@gmail.com>
2022-06-29 00:54:10 +0700

    add solid icons and logo icon

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -11,9 +11,8 @@
 
     <!-- CSS -->
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="<?= url('/termicons/CSS/termicons.min.css')?>">
+    <link rel="stylesheet" href="<?= url('/termicons/css/termicons.min.css')?>">
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="https://termicons.com/CSS/termicons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
 
